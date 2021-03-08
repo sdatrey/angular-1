ca302b70bfdc99b829885a78ac8142ac78435468
sdatrey <shouryadevatrey@gmail.com>
2021-03-08 15:04:26 +0530

    First commit

---
+++ src/app/shopping-list/shopping.service.ts
@@ -0,0 +1,25 @@
+import { Ingredient } from '../shared/ingredient.model';
+import { Subject } from 'rxjs';
+
+export class ShoppingListService{
+    ingredientsChanged = new Subject<Ingredient[]>();
+    ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10),
+      ];
+    
+     onIngredients() {
+        return this.ingredients.slice();
+      }
+      addIngredient(ingredient: Ingredient){
+          this.ingredients.push(ingredient);
+          this.ingredientsChanged.next(this.ingredients.slice());
+      }
+      addIngredients(ingredients: Ingredient[]){
+        // for(let ingredient of ingredients ){
+        //   this.addIngredients(ingredient);
+        // }
+        this.ingredients.push(...ingredients);
+        this.ingredientsChanged.next(this.ingredients.slice());
+      }
+}
